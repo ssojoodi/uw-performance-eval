ee19aa0af9742cbd0c2c9df8ee315e9960e99c49
Sahand Sojoodi <sahand@8090.inc>
2026-05-20 21:50:14 -0400

    Prepare Docker configuration for deployment.

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-RUN python manage.py collectstatic --noinput
+RUN DEBUG=false python manage.py collectstatic --noinput
 
 EXPOSE 8000
 
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -eu
 
+sqlite_dir="$(dirname "${SQLITE_PATH:-/app/db.sqlite3}")"
+mkdir -p "$sqlite_dir"
+
 python manage.py migrate --noinput
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
-
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --worker-class gthread \
+  --workers "${GUNICORN_WORKERS:-2}" \
+  --threads "${GUNICORN_THREADS:-4}" \
+  --timeout "${GUNICORN_TIMEOUT:-60}" \
+  --access-logfile - \
+  --error-logfile -
